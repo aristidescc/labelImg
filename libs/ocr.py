c4be36b4a35b9dea3ae4f144e2b3d626c3f2ed8c
Aristides Castillo Colmenarez <aristides.castillo@bci.cl>
2021-02-19 10:25:58 -0300

    Added custom options to pytesseract to improve OCR capabilities

--- libs/ocr.py
+++ libs/ocr.py
@@ -6,6 +6,10 @@ def scanShape(filePath, shape):
   # Load file from original path
   img_cv2 = cv2.imread(filePath)
   img_rgb = cv2.cvtColor(img_cv2, cv2.COLOR_BGR2RGB)
+  #gray = cv2.cvtColor(img_cv2, cv2.COLOR_BGR2GRAY)
+  #blur = cv2.GaussianBlur(gray, (9,9), 0)
+  #thresh = cv2.adaptiveThreshold(blur,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,30)
+  
   # extract specific portion of the image
   (min_x, min_y, max_x, max_y) = shape.vertices()
   min_x = math.floor(min_x)
@@ -14,7 +18,8 @@ def scanShape(filePath, shape):
   max_y = math.ceil(max_y)
   img_shape = img_rgb[min_y:max_y, min_x:max_x]
   # process image shape with tesseract   
-  result = pytesseract.image_to_string(img_shape).strip()
+  custom_options = r'--psm 10 --psm 6 --oem 3'
+  result = pytesseract.image_to_string(img_shape, lang='spa', config=custom_options).strip()
   shape.value = result
   # return scanned text
   return result 
