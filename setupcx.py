c5cd037ba59969b3a7a1c513926f8e3715f4b8e2
Aristides Castillo Colmenarez <aristides.castillo@bci.cl>
2021-03-09 22:15:09 -0300

    Added CX Freeze setup, and fixed predefined_classes file

---
+++ setupcx.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+build_options = {'packages': ["os"], 'excludes': [], "include_files": ["data"]}
+
+base = 'Console'
+
+executables = [
+    Executable('labelImg.py', base=base, target_name = 'LabelImg')
+]
+
+setup(name='LabelImg',
+      version = '1.0',
+      description = 'Aplicación para etiquetado de imágenes, especialmente preparado para etiquetado de documentos.',
+      options = {'build_exe': build_options},
+      executables = executables)
